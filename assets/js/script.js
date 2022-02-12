5f5e792d36682521e6d95b9d45ea1b4d83b5329e
Seun Ajetomobi <solaajetomobi@aol.com>
2022-02-12 02:39:04 +0000

    Added animated background style through css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,49 @@
-//declaration of letiables by getting them by selector
+//declaration of variables by getting them by selector
 let days = document.querySelector("#days");
 let hours = document.querySelector("#hours");
 let minutes = document.querySelector("#minutes");
 let seconds = document.querySelector("#seconds");
 let countdown = document.querySelector("#countdown");
 
-//get the current year
+//finding the current year
 let date = new Date();
 let year = date.getFullYear();
 
-//set the new year date and time
+//finding the value of the dates and time of the new year
 let newYearTime = new Date(`January 01 ${year + 1} 00:00:00`);
 
 // Countdown update time
 function countdownUpdate() {
-  let currentTime = new Date();
+  let today = new Date();
 
-  let day = Math.floor((newYearTime - currentTime)/1000/60/60/24);
-  let hour = Math.floor((newYearTime - currentTime)/1000/60/60) % 24;
-  console.log(hour);
-  let minute = Math.floor((newYearTime - currentTime)/1000/60) % 60;
-  let second = Math.floor((newYearTime - currentTime)/1000) % 60;
-  
+/* finding the interval between newYearTime 
+and today, divided by 1000 milliseconds and
+using the result to calculate the number of
+days, hours, minutes and seconds and using 
+the Math.floor Method to round up the result 
+to a whole number or integer.And appending 
+the results to the HTML DOM.
+*/
 
-  // Add values to DOM
-  days.innerHTML = day;
-  hours.innerHTML = hour //< 10 ? "0" + hour : hour;
-  minutes.innerHTML = minute //< 10 ? "0" + minute : minute;
-  seconds.innerHTML = second //< 10 ? "0" + second : second;
+  let day = Math.floor((newYearTime - today)/1000/60/60/24);
+  document.getElementById('days').innerHTML = day; 
+
+  let hour = Math.floor((newYearTime - today)/1000/60/60) % 24;
+  document.getElementById('hours').innerHTML = hour;
+
+  let minute = Math.floor((newYearTime - today)/1000/60) % 60;
+  document.getElementById('minutes').innerHTML = minute;
+
+  let second = Math.floor((newYearTime - today)/1000) % 60;  
+  document.getElementById('seconds').innerHTML = second; 
 }
 
-// Run every second
-setInterval(countdownUpdate, 1000);
+//setInterval method continues calling the function countdownUpdate.
+newYearCountdown = setInterval(countdownUpdate, 1000);
+
+// to print out HAPPY NEW YEAR...
+if (newYearTime = new Date(`January 01 ${year + 1} 00:00:00`) ){
+
+} else {
+
+};
